8408cda98156298fbd92138882361d2e21f03639
Changuk Choi <devchoi1213@gmail.com>
2023-11-11 23:31:08 +0900

    main.ts 로직 수정
    bootstrap 시간을 handler 밖에서 처리하도록 해서 cold start의 시간을 단축

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import serverlessExpress from '@vendia/serverless-express';
-import { Callback, Context, Handler } from 'aws-lambda';
+import { Callback, Context, Handler, APIGatewayEvent } from 'aws-lambda';
 import { AppModule } from './app.module';
+import { ReplaySubject, firstValueFrom } from 'rxjs';
 
-let server: Handler;
+//Declare a ReplaySubject to store the serverlessExpress instance.
+const serverSubject = new ReplaySubject<Handler>()
 
 async function bootstrap(): Promise<Handler> {
-  const app = await NestFactory.create(AppModule);
-  await app.init();
+	console.log('COLD START: Initializing Nest');
+	const app = await NestFactory.create(AppModule);
+	await app.init();
 
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
+	const expressApp = app.getHttpAdapter().getInstance();
+	return serverlessExpress({ app: expressApp });
 }
 
+//Do not wait for lambdaHandler to be called before bootstraping Nest.
+//Pass the result of bootstrap() into the ReplaySubject
+bootstrap().then(server => serverSubject.next(server))
+
 export const handler: Handler = async (
-  event: any,
-  context: Context,
-  callback: Callback,
+	event: APIGatewayEvent,
+	context: Context,
+	callback: Callback,
 ) => {
-  server = server ?? (await bootstrap());
-  return server(event, context, callback);
+	//Convert the ReplaySubject to a Promise.
+	//Wait for bootstrap to finish, then start handling requests.
+	const server = await firstValueFrom(serverSubject)
+	return server(event, context, callback);
 };
